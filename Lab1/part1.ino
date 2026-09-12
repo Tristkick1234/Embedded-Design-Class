952cd5f9e92cfd99b740801f7c510c867252884f
SijuolaSamaye <sijuolas7@gmail.com>
2026-09-12 02:01:10 -0400

    Update part1.ino

--- Lab1/part1.ino
+++ Lab1/part1.ino
@@ -1,12 +1,16 @@
-int LED_PIN = 11;  // Data pin connected to onboard RGB LED for ESP32-c6
+int BUTTON = 11;  // Data pin connected to onboard RGB LED for ESP32-c6
+int LED = 7;
+
 void setup() {
   //init GPIO pin and serial monitor
   Serial.begin(9600);
-  pinMode(LED_PIN, INPUT_PULLDOWN);
+  pinMode(BUTTON, INPUT_PULLDOWN);
+  pinMode(LED, OUTPUT);
 }
 
 void loop() {
   //Read values from pin 11. drive one side of button high.
-  int button = digitalRead(LED_PIN);
+  int button = digitalRead(BUTTON);
+  digitalWrite(LED, button);
   Serial.println(button);
 }
