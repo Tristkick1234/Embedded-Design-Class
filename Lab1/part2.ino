16d6a55aa8910920caa5a183ee1d53fac18d1c6f
SijuolaSamaye <sijuolas7@gmail.com>
2026-09-14 21:10:25 -0400

    Create part2.ino

---
+++ Lab1/part2.ino
@@ -0,0 +1,6 @@
+// initializing this registers
+int* GPIO_OUTPUT_EN = (int*) 0x60091024; // GPIO_ENABLE_W1TS_REG
+int* GPIO_OUTPUT_SET = (int*) 0x60091008; //GPIO_OUT_W1TS_REG
+int* GPIO_OUTPUT_CLR = (int*) 0x6009100C; // clear register
+int* GPIO_INPUT = (int*) 0x60091003C; //input
+
