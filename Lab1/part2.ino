1d8e3a639229e60e9ca6644f1e48e6e6f1e2ecc9
SijuolaSamaye <sijuolas7@gmail.com>
2026-09-15 20:34:56 -0400

    Update part2.ino

--- Lab1/part2.ino
+++ Lab1/part2.ino
@@ -7,7 +7,8 @@ int* IO_MUX_GPIO11 = (int*) 0x60090030;
 
 void setup() {
   *GPIO_OUTPUT_EN = (1 << 7); //setting the output to be gpio7
-  *IO_MUX_GPIO11 = (1 << 9) | (1 << 7); // ORs the input en and input pulldown bit
+  *IO_MUX_GPIO11 |=  (1 << 9) | (1 << 7);
+  *IO_MUX_GPIO11 &= ~(1 << 8);
 }
 
 void loop() {
